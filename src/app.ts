881229fca0e16c6e818333598c01d4cc92d66762
Danskuvv <danielvv@metropolia.fi>
2024-09-12 15:32:30 +0300

    initial commit

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,19 @@
-import dotenv from 'dotenv';
-dotenv.config();
-import express, {Request, Response} from 'express';
-import morgan from 'morgan';
-import helmet from 'helmet';
+import 'dotenv/config';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
-
+import helmet from 'helmet';
+import morgan from 'morgan';
+import { createProxyMiddleware, Options } from 'http-proxy-middleware';
+import {ClientRequest} from 'http';
 import {notFound, errorHandler} from './middlewares';
-import api from './api';
 import {MessageResponse} from './types/Messages';
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan('combined'));
 app.use(helmet());
 app.use(cors());
+app.disable('x-powered-by');
 app.use(express.json());
 
 app.get<{}, MessageResponse>('/', (_req: Request, res: Response) => {
@@ -22,7 +22,44 @@ app.get<{}, MessageResponse>('/', (_req: Request, res: Response) => {
   });
 });
 
-app.use('/api/v1', api);
+interface ExtendedOptions extends Options {
+  onProxyReq?: (proxyReq: ClientRequest) => void;
+}
+
+const services = [
+  {
+    route: '/api1',
+    target: 'https://jsonplaceholder.typicode.com/posts',
+  },
+  {
+    route: '/api2',
+    target: 'https://catfact.ninja/fact',
+  },
+  {
+    route: '/api3',
+    target: 'https://api.openweathermap.org/data/2.5/weather?q=London',
+    on: {
+      onProxyReq: (proxyReq: ClientRequest) => {
+      // Append apikey query parameter to the target URL
+      proxyReq.path += '&appid=f3e0b30bb708435af79fa709d74b0750';
+      console.log('Request path:', proxyReq.path);
+    },
+  },
+}];
+
+services.forEach(({ route, target, on }) => {
+  const proxyOptions = {
+    on,
+    target,
+    changeOrigin: true,
+    pathRewrite: {
+      [`^${route}`]: '',
+    },
+    secure: process.env.NODE_ENV === 'production', // Enable SSL verification in production
+  };
+
+  app.use(route, createProxyMiddleware(proxyOptions as ExtendedOptions));
+});
 
 app.use(notFound);
 app.use(errorHandler);
